cf629dcd6247afbadd5ac9838698dcd4aabc4d02
Rachel Kat <rachel.kat@koho.ca>
2020-12-07 23:24:24 -0500

    Add Framer Motion to contact us icons and url property (#5)
    * add framer motion and links to contact us
    
    * back to single quotes

--- src/components/contact-info.tsx
+++ src/components/contact-info.tsx
@@ -1,22 +1,28 @@
 import * as React from 'react';
-
+import { motion } from 'framer-motion';
 
 export interface ContactInfoProps {
   icon: string;
   title: string;
   content: string;
+  url: string;
 }
 
-export const ContactInfo = (
-  props: ContactInfoProps,
-): JSX.Element => {
+export const ContactInfo = (props: ContactInfoProps): JSX.Element => {
   return (
     <div className={'flex flex-column ma4'}>
       <div>
-        <i className={`fa fa-${props.icon} fa-2x v-mid`} aria-hidden='true' />
-        <span className={'f3 v-mid pl2 tu'}>{props.title}</span>
+        <motion.div
+          className={`fa fa-${props.icon} fa-2x v-mid`}
+          aria-hidden='true'
+          whileHover={{ scale: 1.2, rotate: 90 }}
+          whileTap={{ scale: 0.8, rotate: -90, borderRadius: '100%' }}
+        />
+        <a href={props.url} className={'f3 v-mid pl2 tu'}>
+          {props.title}
+        </a>
       </div>
       <p className={'lh-copy'}> {props.content} </p>
     </div>
   );
-}
+};
--- src/layouts/contact-us.tsx
+++ src/layouts/contact-us.tsx
@@ -1,11 +1,6 @@
 import * as React from 'react';
 
-import {
-  Header,
-  Headline,
-  ContactInfo,
-} from '../components';
-
+import { Header, Headline, ContactInfo } from '../components';
 
 export class ContactUs extends React.Component {
   public render(): JSX.Element {
@@ -14,35 +9,59 @@ export class ContactUs extends React.Component {
         <Header />
         <Headline
           mainText={'We\'d love to hear from you!'}
-          supportingText={'Whether you have a question or have an idea to share with us, ...'}
+          supportingText={
+            'Whether you have a question or have an idea to share with us, ...'
+          }
         />
         <div className={'flex flex-column items-center pt5'}>
-          <div className={'flex flex-column flex-row-m flex-row-l justify-around items-center'}>
+          <div
+            className={
+              'flex flex-column flex-row-m flex-row-l justify-around items-center'
+            }
+          >
             <ContactInfo
               icon={'meetup'}
               title={'Meetup'}
-              content={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'}
+              url={
+                'https://www.meetup.com/toronto-software-development-meetup-group/'
+              }
+              content={
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'
+              }
             />
             <ContactInfo
               icon={'twitter'}
               title={'Twitter'}
-              content={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'}
+              url={'https://twitter.com/wwgtoronto'}
+              content={
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'
+              }
             />
           </div>
-          <div className={'flex flex-column flex-row-m flex-row-l justify-around items-center'}>
+          <div
+            className={
+              'flex flex-column flex-row-m flex-row-l justify-around items-center'
+            }
+          >
             <ContactInfo
               icon={'envelope'}
               title={'Email'}
-              content={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'}
+              url={'mailto:wwgtoronto@gmail.com'}
+              content={
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'
+              }
             />
             <ContactInfo
               icon={'slack'}
               title={'Slack'}
-              content={'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'}
+              url={'#'}
+              content={
+                'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus recusandae sit repudiandae debitis est voluptates omnis harum. Ipsam nobis, qui animi a dicta nesciunt placeat, necessitatibus aut id officiis nihil.'
+              }
             />
           </div>
-        </div >
-      </ div >
+        </div>
+      </div>
     );
   }
 }
