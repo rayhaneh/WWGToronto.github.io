2523ec6393d0510c4a775e5da0d00a284ae239fc
Rayhaneh <r.b.assady@gmail.com>
2020-12-12 23:47:35 -0500

    Update first event

--- src/components/event-info.tsx
+++ src/components/event-info.tsx
@@ -9,21 +9,28 @@ export interface EventInfoProps {
   signUpLink: string;
 }
 
+const maxLength = 400
+
 export const EventInfo = (
   props: EventInfoProps,
 ): JSX.Element => {
+  const trimmedContent = props.content.length > maxLength ?
+    props.content.substring(0, maxLength - 3) + '... ' :
+    props.content;
+
   return (
-    <a href={props.signUpLink} className={'link black dim db'}>
-      <div className={'flex flex-column flex-row-m flex-row-l justify-center pa3 mt4'}>
-        <div className={'flex flex-column justify-center w-100 w-30-m w-20-l white bg-gold tc'}>
-          <h1 className={'f-subheadline'}>{props.day}</h1>
-          <p className={'ttu b'}>{props.month} {props.year}</p>
-        </div>
-        <div className={'flex flex-column pa4 w-100 w-70-m w-80-l bg-near-white'}>
-          <h1 className={'ttu b f4 v-mid fw4'}>{props.title}</h1>
-          <p className={'lh-copy'}>{props.content}</p>
+    <div className={'flex flex-column flex-row-m flex-row-l justify-center pa3 mt4'}>
+      <div className={'flex flex-column justify-center w-100 w-30-m w-20-l white bg-gold tc'}>
+        <h1 className={'f-subheadline'}>{props.day}</h1>
+        <p className={'ttu b lh-solid truncate h-25'}>{props.month} {props.year}</p>
+      </div>
+      <div className={'flex flex-column pa4 w-100 w-70-m w-80-l bg-near-white'}>
+        <h1 className={'ttu b f4 v-mid fw4'}>{props.title}</h1>
+        <div className={'lh-copy'}>
+          <span>{trimmedContent}</span>
+          <a href={props.signUpLink} className={'link gray dim i'}>sign up</a>
         </div>
       </div>
-    </a>
+    </div>
   );
 }
--- src/layouts/events.tsx
+++ src/layouts/events.tsx
@@ -11,20 +11,20 @@ export class Events extends React.Component {
     return (
       <>
         <Header />
-        <div className={'pt5 bg-light-gray'}>
+        <div className={'pv5 bg-light-gray'}>
           <div className={'flex flex-column'}>
             <h1 className={'mt5 tc fw2 mid-gray'}>Upcoming Events</h1>
+            <p className={'tc'}>
+              For a list of past events please visit our <a href={'https://www.meetup.com/women-who-go-toronto/events'} className={'link gray dim'}>meetup</a> page
+            </p>
             <EventInfo
               day={20}
               month={'Jan'}
               year={2020}
-              title={'First Event'}
-              content={'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae mollitia alias nesciunt accusantium optio commodi, blanditiis soluta hic ad in voluptate dolores itaque fugiat provident modi porro. Labore, totam ullam. Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repudiandae mollitia alias nesciunt accusantium optio commodi, blanditiis soluta hic ad in voluptate dolores itaque fugiat provident modi porro. Labore, totam ullam.'}
-              signUpLink={'https://meetup.com/women-who-go-toronto/'}
+              title={'Reading club: Go best practices, six years in'}
+              content={"Let's start off simple with a single article for a nice introduction! The article we are going to read is 'Go best practices, six years in' by Peter Bourgon. The author, Peter Bourgon, has previously worked at SoundCloud and Weaveworks and is now a Principal Engineer at Fastly. This article was published in 2016 as an adaptation to a talk from 2014 and is included in the popular Gopher Reading List. This article goes over an array of practical topics like testing, IDEs, program design, formatting, and more. The writing isn't as prescriptive as you might initially assume it would be and presents the information more like observations and learnings."}
+              signUpLink={'https://www.meetup.com/women-who-go-toronto/events/275055127'}
             />
-          </div>
-          <div className={'pa3'}>
-            <p>For a list of past events please visit our <a href={'https://www.meetup.com/women-who-go-toronto/events'} className={'link gray dim'}>meetup</a> page.</p>
           </div>
         </div>
         <Footer />
